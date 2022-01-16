0501ed5cbb04b7569ae70bcd7593ab95d37260dc
soucre.thaipvt <soucre.thaipvt@gmail.com>
2022-01-16 20:52:54 +0700

    truoc khi xoa list map, va bat dau tao flatlist

--- App.js
+++ App.js
@@ -1,49 +1,61 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 import {
   StyleSheet,
   TouchableOpacity,
   Text,
   View,
   ScrollView,
-} from 'react-native'
-import CardListItem from './src/components/CardListItem'
-import images from './src/res/images'
+  FlatList,
+} from 'react-native';
+import { keyExtractor } from 'react-native/Libraries/Lists/VirtualizeUtils';
+import CardListItem from './src/components/CardListItem';
+import images from './src/res/images';
 
 export default class App extends Component {
   state = {
-    count: 0
-  }
+    count: 0,
+    listItems: [
+      {id: 1, title: 'Item 1', image: images.fr_home11x},
+      {id: 2, title: 'Item 2', image: images.fr_home21x},
+      {id: 3, title: 'Item 3', image: images.fr_home31x},
+      {id: 4, title: 'Item 4', image: images.fr_home41x},
+      {id: 5, title: 'Item 5', image: images.fr_home51x},
+      {id: 6, title: 'Item 6', image: images.fr_home61x},
+    ],
+  };
 
   onPress = () => {
-    console.log('erro'+this.state.count);
+    console.log('erro' + this.state.count);
     this.setState({
-        
-      count: this.state.count + 1
-    })
-  }
+      count: this.state.count + 1,
+    });
+  };
 
- render() {
-    console.log('erro render'+this.state.count);
+  render() {
+    console.log('erro render' + this.state.count);
+    const {listItems} = this.state;
     return (
-      <View >
-      <ScrollView  style={{paddingLeft:15, paddingRight:15}} contentContainerStyle={{backgroundColor:'#F00'}}>
-        <TouchableOpacity
-         style={styles.button}
-         onPress={this.onPress}
-        >
-         <Text>Click me</Text>
-        </TouchableOpacity>
-        <View>
-          <Text>
-            You clicked { this.state.count } times
-          </Text>
-          <CardListItem title='ahihi' images={images.fr_home11x}/>
-          <CardListItem title='ahihi' images={images.fr_home21x}/>
-          <CardListItem title='ahihi' images={images.fr_home21x}/>
-        </View>
+      <View>
+        <ScrollView
+          style={{paddingLeft: 15, paddingRight: 15}}
+          contentContainerStyle={{backgroundColor: '#F00'}}>
+          <TouchableOpacity style={styles.button} onPress={this.onPress}>
+            <Text>Click me</Text>
+          </TouchableOpacity>
+          <View>
+            <Text>You clicked {this.state.count} times</Text>
+            {listItems.map(itemCard=>(
+                <CardListItem key={itemCard.id} itemCard={itemCard} />,
+                console.log('list map id ', this.state.listItems)
+            ))}
+            <FlatList data={listItems}
+                renderItem={({ item })=> <CardListItem itemCard={item} />}
+                keyExtractor={(item)=> itemCard.id}
+            />
+          </View>
         </ScrollView>
       </View>
-    )
+    );
   }
 }
 
@@ -57,7 +69,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#DDDDDD',
     padding: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
-
-})
+});
